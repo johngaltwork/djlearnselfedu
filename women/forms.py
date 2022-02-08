db5744acf00588c14185e178f32968df2883314d
johngaltwork <john.galt.work@gmail.com>
2022-02-08 20:40:52 +0200

    20220208-1

--- women/forms.py
+++ women/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import *
 
@@ -23,3 +26,19 @@ class AddPostForm(forms.ModelForm):
             raise ValidationError('Длина превышает 200 символов')
 
         return title
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User  # стандартная модель джанго, которая работает с таблицей базы данных uath_user
+        fields = ('username', 'email', 'password1', 'password2')  # поля, которые будут отображаться
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- women/views.py
+++ women/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -19,6 +22,7 @@ class WomenHome(DataMixin, ListView):
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Главная страница')
         return dict(list(context.items()) + list(c_def.items()))
@@ -59,6 +63,7 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     raise_exception = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Добавление статьи')
         return dict(list(context.items()) + list(c_def.items()))
@@ -79,8 +84,8 @@ def contact(request):
     return HttpResponse('Обратная связь')
 
 
-def login(request):
-    return HttpResponse('Авторизация')
+# def login(request):
+#     return HttpResponse('Авторизация')
 
 
 class ShowPost(DataMixin, DetailView):
@@ -91,6 +96,7 @@ class ShowPost(DataMixin, DetailView):
     context_object_name = 'post'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
         context = super().get_context_data(**kwargs)
         context['menu'] = menu
         c_def = self.get_user_context(title=context['post'])
@@ -119,6 +125,7 @@ class WomenCategory(DataMixin, ListView):
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
                                       cat_selected=context['posts'][0].cat_id)
@@ -142,3 +149,45 @@ class WomenCategory(DataMixin, ListView):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm  #  форма для регистрации новых пользователей
+    template_name = 'women/register.html'  # ссылка на шаблон
+    success_url = reverse_lazy('login')  # перенаправление на url адрес при успешной регистрации
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+    def form_valid(self, form):
+        '''Метод вызывается при успешной проверке формы регистрации,
+        сохраняет данные в базу данных и авторизоывает пользователя,
+        перенаправляя на главную страницу'''
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'  # ссылка на шаблон
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        '''Метод для формирования контекста для шаблона отображения'''
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        '''Метод вызывается, если пользователь ввел правильно логин и пароль'''
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    '''Функция вызывает стандартный django метод logout, для выхода пользователя из авторизации'''
+    logout(request)
+    return redirect('login')
